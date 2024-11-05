291eb593bb22288667ec9fbdd25f9c6f19c724b1
Gustavo Aniceto <gpradofe@nd.edu>
2024-11-05 02:57:39 +0000

    a

---
+++ QueryAPI/QueryAPIKernel/1.1-Presentation/Query.API.Kernel/Startup.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Query.API.Kernel.Service.Core.APIBASE.StartupConfiguration;
+using Query.API.Kernel.Service.Core.APIBASE.StartupConfiguration.Swagger;
+using Query.API.Kernel.StartupConfiguration;
+namespace Query.API.Kernel
+{
+    public class Startup
+    {
+        #region Constructor
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+        }
+        #endregion
+        #region Attributes
+        public IConfiguration _configuration { get; }
+        #endregion
+        #region Methods
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddApiVersioning(ver =>
+            {
+                ver.ReportApiVersions = true;
+                ver.AssumeDefaultVersionWhenUnspecified = true;
+            });
+            services.AddVersionedApiExplorer(ver =>
+            {
+                ver.SubstituteApiVersionInUrl = true;
+            });
+            services.AddExtensions(_configuration);
+
+            services.AddMvc(setupAction =>
+            {
+                setupAction.EnableEndpointRouting = false;
+            }).AddJsonOptions(jsonOptions =>
+            {
+                jsonOptions.JsonSerializerOptions.PropertyNamingPolicy = null;
+            });
+            services.AddQueryApplication(_configuration);
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin();
+                    });
+            });
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApiVersionDescriptionProvider provider)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseHealthChecks("/healthycheck");
+            app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true)
+                .AllowCredentials());
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.AddSwaggers(provider);
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+        }
+        #endregion
+
+    }
+}
---
+++ QueryAPI/QueryAPIKernel/4-Domain/Query.API.Kernel.Domain.Contracts/DTOs/SendQueryDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Query.API.Kernel.Domain.Contracts.DTOs
+{
+    public class SendQueryDTO
+    {
+        public string query { get; set; }
+        public string hash { get; set; }
+    }
+}

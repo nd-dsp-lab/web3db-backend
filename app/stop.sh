dff049cb7f9b0724e71b27b181320801ebcbe52a
Showkot Hossain <showkot.csedu@gmail.com>
2025-05-22 01:44:52 +0000

    file remove

--- app/stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Stopping FastAPI/Spark-local (SGX) and IPFS containers..."
-sudo docker compose -f driver.yml down --remove-orphans
-
-echo "All relevant containers have been stopped and removed."
-# Optional: Clean up volumes if desired, but be careful with data.
-# echo "To remove IPFS data volume, run: sudo docker volume rm <projectname>_ipfs_data"

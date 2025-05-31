0bbcb35ec2c96b0c8dcdfaa27a05aea018b1ac66
Showkot Hossain <showkot.csedu@gmail.com>
2025-05-31 11:14:33 -0400

    remove ipfs data when stop services

--- app/stop.sh
+++ app/stop.sh
@@ -7,5 +7,5 @@ sudo docker compose -f spark-master.yml down
 # Stop and remove containers defined in driver.yml
 echo "Stopping Spark driver containers..."
 sudo docker compose -f driver.yml down
-
+sudo rm -rf data
 echo "All containers have been stopped successfully."

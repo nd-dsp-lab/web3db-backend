6d3a2e27214b9b4f9fd665f16554a000488d1246
Showkot Hossain <showkot.csedu@gmail.com>
2025-11-13 20:36:21 +0000

    port change

--- app/scripts/app.py
+++ app/scripts/app.py
@@ -2759,4 +2759,4 @@ def shutdown_event():
 if __name__ == "__main__":
     import uvicorn
     logger.info("Starting FastAPI server...")
-    uvicorn.run(app, host="0.0.0.0", port=8001, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
